8a34b25532826454cfb40e45079bb76f46d064ab
Jack Hausmann <76900681+reccejack@users.noreply.github.com>
2025-11-15 10:05:20 +0900

    Add project files.

---
+++ TempGauge-MyICSv1/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TempGauge_MyICSv1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
