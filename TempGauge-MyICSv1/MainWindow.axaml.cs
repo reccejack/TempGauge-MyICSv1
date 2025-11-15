3c33234c9171eaf897d70e3b7ba86d688a248bfb
Jack Hausmann <76900681+reccejack@users.noreply.github.com>
2025-11-15 22:35:17 +0900

    TCP and Serial connection classes established.  Working out bugs regarding full connectivity with both.

--- TempGauge-MyICSv1/MainWindow.axaml.cs
+++ TempGauge-MyICSv1/MainWindow.axaml.cs
@@ -1,12 +1,27 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.IO.Ports;
+using TempGauge;
 
 namespace TempGauge_MyICSv1
 {
     public partial class MainWindow : Window
     {
+
+        TcpConnection tcp1 = new TcpConnection();
+        //SerialConnection sensor1 = new SerialConnection("COM3", 9600);
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Start_Click1(object? sender, RoutedEventArgs e)
+        {            
+            tcp1.StartServer();
+            tcp1.Sense(TempTextBlock);
+            //sensor1.Sense(TempTextBlock);
+
         }
     }
 }
---
+++ TempGauge-MyICSv1/TcpConnection.cs
@@ -0,0 +1,88 @@
+﻿using Avalonia.Controls;
+using Avalonia.Threading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TempGauge
+{
+    class TcpConnection
+{
+    public static float data = 0;
+    public static byte[]? bytes = null;
+
+    static public bool AppState = false;
+    public void StartServer()
+    {
+        IPAddress ipAddress = IPAddress.Parse("169.254.102.3");
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 8888);
+
+        try
+        {
+            Socket listener = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+            Socket handler = listener.Accept();
+            
+            while (true)
+            {
+                bytes = new byte[10];
+                int bytesRec = handler.Receive(bytes);
+                data = float.Parse(bytesRec.ToString());
+            }
+        }
+        catch (Exception e)
+        {
+                //Insert TEXTBOX that provides information on this exception
+                TextBlock msg = new TextBlock();
+                msg.Text = e.ToString();
+                UpdateMessage(data, msg);
+            }        
+    }
+
+    public void Sense(TextBlock TempTxtblock)
+    {
+        new Thread(() =>
+        {
+            StartServer();
+            TaskCompletionSource<bool> taskComplete = new TaskCompletionSource<bool>();
+            while (AppState == false)
+            {
+                try
+                {
+                    UpdateMessage(data, TempTxtblock);
+                }
+                catch (System.InvalidOperationException e)
+                {
+                    /*ignored exception*/
+                    TextBlock msg = new TextBlock();
+                    msg.Text = e.ToString();
+                    UpdateMessage(data, msg);
+                }
+
+                if (AppState == true)
+                {
+                    taskComplete.SetResult(true);
+                    Action action3 = () => TempTxtblock.Text = "OFF";
+                    //port.Close();
+                    Dispatcher.UIThread.Post(action3);
+                    break;
+                }
+            }
+        }).Start();
+    }
+
+    void UpdateMessage(float inputFloat, TextBlock textBlock)
+    {
+        Action action1 = () => textBlock.Text = inputFloat.ToString("F1");      
+        Dispatcher.UIThread.Post(action1);        
+    }
+}
+}
